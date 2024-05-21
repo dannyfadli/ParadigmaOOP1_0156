20e8bea9bfc05de1eb8b1726c43fdc52bf44d58f
dannyfadli <danyfdly95@gmail.com>
2024-05-21 11:26:12 +0700

    menampilka ke layar ada anak dan tidak ada

--- mainA/ibu.h
+++ mainA/ibu.h
@@ -4,6 +4,18 @@
 class ibu
 {
 public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama)
+	{
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu()
+	{
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+
 };
 
 #endif // !IBU_H
