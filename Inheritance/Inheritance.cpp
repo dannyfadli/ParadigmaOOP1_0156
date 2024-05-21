bafcedebc1d8d94dae840c54c5f40b0c1a2be8e5
dannyfadli <danyfdly95@gmail.com>
2024-05-21 19:44:57 +0700

    menyelesaiken  intheritance

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -24,10 +24,29 @@ public:
 
 };
 
+class pelajar : public orang
+{
+public:
+    string sekolah;
 
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar()
+    {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perknalan()
+    {
+        return "Hallo, Nama Saya" + nama + "Dari Sekolah " + sekolah + "\n\n"
+    }
 
 int main()
 {
-    
+    pelajar siswa1("Andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perknalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) <<  endl;
+    return 0;
 }
 
