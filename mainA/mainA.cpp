b488f20e0403e3d3e102c5ca6a4712927acb6065
dannyfadli <danyfdly95@gmail.com>
2024-05-21 12:02:54 +0700

    menyelesaikan  typo

--- mainA/mainA.cpp
+++ mainA/mainA.cpp
@@ -10,22 +10,22 @@ int main()
     ibu* varIBu = new ibu("dini");
     ibu* varibu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
-    anak* varaAnak2 = new anak("rini");
+    anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
     varIBu->tambahAnak(varAnak1);
-    varIBu->tambahAnak(varaAnak2);
+    varIBu->tambahAnak(varAnak2);
     varibu2->tambahAnak(varAnak3);
     varibu2->tambahAnak(varAnak1);
 
 
     varIBu->cetakAnak();
-    varIBu->cetakAnak();
+    varibu2->cetakAnak();
 
     delete varIBu;
     delete varibu2;
     delete varAnak1;
-    delete varaAnak2;
+    delete varAnak2;
     delete varAnak3;
 
     return 0;
