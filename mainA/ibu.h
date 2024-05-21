70bbb3f26bc41f7e4579c934984a960a7b62d509
dannyfadli <danyfdly95@gmail.com>
2024-05-21 11:30:40 +0700

    menyelesaikan header ibu

--- mainA/ibu.h
+++ mainA/ibu.h
@@ -16,7 +16,18 @@ public:
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
 
+	void tambahAnak(anak*);
+	void cetakANak();
 };
+void ibu::tambahAnak()
+{
+	cout << "Daftar Anak dari Ibu\"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak)
+	{
+		cout << a->nama << "\n";
+	}
+}
+
 
 #endif // !IBU_H
 
